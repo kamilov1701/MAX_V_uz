ea67eae82d19691c6c0f461eb08e74a489520715
azizbek_k <azizbekkamilov1701@gmail.com>
2026-07-24 00:54:29 +0500

    Navigation started by Azizbek

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import Navlogo from "../assets/MAX_-_LOAN-logo.png";
 
 function Navbar() {
     return (
         <nav>
-            <Link to="/">Home</Link> |{" "}
-            <Link to="/about">About</Link> |{" "}
-            <Link to="/projects">Projects</Link> |{" "}
-            <Link to="/contact">Contact</Link>
+            <img src={Navlogo} alt="" />
+            <div className="container nav_content">
+                <Link to="/">Home</Link> |{" "}
+                <Link to="/about">About</Link> |{" "}
+                <Link to="/projects">Projects</Link> |{" "}
+                <Link to="/contact">Contact</Link>
+            </div>
+            <a href="#!"><button>Contact with US</button></a>
         </nav>
     );
 }
