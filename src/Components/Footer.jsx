da7751dd17e32d0728387273507fd13a52e4859a
giyosbekganibekov <giyosbekganiybekov2011@gmail.com>
2026-07-24 10:12:14 +0500

    Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,27 @@
 function Footer() {
     return (
-        <footer>
-            <h3>© 2026 Azizbek Kamilov</h3>
+        <footer className="bg-[url(./src/assets/Footer_photo.png)] bg-cover bg-center w-full h-[200px]" >
+            <div className="flex justify-center items-center h-full">
+                <div className="flex">
+                    <div>
+                        <img className="h-[200px] w-auto" src="src/assets/MAX_-_LOAN-logo.png" alt="Logo" />
+                    </div>
+                    <div>
+                        <h1 className="text-blue-500 text-[75px] font-bold">MAX - V.UZ</h1>
+                        <div className="flex items-center">
+                            <a className="text-white text-[25px] mr-[5px] flex items-center" href="https://www.instagram.com/max_v.uz">
+                                <img className="h-[40px] w-auto " src="src/assets/instagram.png" alt="Instagram" />
+                                max_v.uz
+                            </a>
+                            <a className="text-white text-[20px] flex items-center " href="https://t.me/MAX_V_uz">
+                                <img className="h-[40px] w-auto ml-[20px] mr-[12px]" src="src/assets/telegram.png" alt="Telegram" />
+                                MAX_V_uz
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 }
