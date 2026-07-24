baaa7dd208510a065c05a16f0bbe030201171d14
azizbek_k <azizbekkamilov1701@gmail.com>
2026-07-24 13:14:03 +0500

    Home page Header Start by Azizbek

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,18 +3,23 @@ import reactLogo from "../assets/react.svg";
 import viteLogo from "../assets/vite.svg";
 import heroImg from "../assets/hero.png";
 import "../App.css";
+import HeaderLogo from "../assets/Header_BG.png";
 
 function Home() {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <section id="center">
-                <div>
-                    <h1>Home Page</h1>
-                    <p>Welcome to my website.</p>
+        <div style={{ backgroundImage: `url(${HeaderLogo})`, backgroundSize: "cover", backgroundPosition: "center", height: "100vh" }}>
+            <div className="container">
+                <div className="header_content max-w-[568px] w-full pt-[90px]">
+                    <h1 className="text-[72px] font-bold text-[#fff]">Building the Future with Code</h1>
+                    <p className="text-[#C2C6D6] text-[18px] max-w-[512px] w-full">We are a team of visionary developers crafting high-performance web applications.</p>
+                    <button><a href="#!">Explore Projects</a></button>
+                    <button><a href="#!">Apply for MAX - V</a></button>
                 </div>
-            </section>
+            </div>
+        </div>
         </>
     );
 }
