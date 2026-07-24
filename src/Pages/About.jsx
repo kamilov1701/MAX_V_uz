8a3ce991a3f8bf906e2dba49fdd7ce7fd3e4d427
giyosbekganibekov <giyosbekganiybekov2011@gmail.com>
2026-07-24 15:59:41 +0500

    About header end

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,42 +1,65 @@
 import { useState, useEffect } from "react";
 
 export default function About() {
-  const [experience, setExperience] = useState(0);
-  const [projects, setProjects] = useState(0);
-  const [clients, setClients] = useState(0);
-  const [tasks, setTasks] = useState(0);
-  const [support, setSupport] = useState(0);
+    const [experience, setExperience] = useState(0);
+    const [projects, setProjects] = useState(0);
+    const [clients, setClients] = useState(0);
+    const [tasks, setTasks] = useState(0);
+    const [support, setSupport] = useState(0);
 
-  useEffect(() => {
-    const duration = 1500; // 👈 Hamma animatsiya tezligi shu yerda
-    const interval = 16;
+    useEffect(() => {
+        const duration = 1500; // 👈 Hamma animatsiya tezligi shu yerda
+        const interval = 16;
 
-    const animate = (end, setter) => {
-      let start = 1;
-      const increment = end / (duration / interval);
+        const animate = (end, setter) => {
+            let start = 1;
+            const increment = end / (duration / interval);
 
-      const timer = setInterval(() => {
-        start += increment;
+            const timer = setInterval(() => {
+                start += increment;
 
-        if (start >= end) {
-          setter(end);
-          clearInterval(timer);
-        } else {
-          setter(Math.floor(start));
-        }
-      }, interval);
-    };
+                if (start >= end) {
+                    setter(end);
+                    clearInterval(timer);
+                } else {
+                    setter(Math.floor(start));
+                }
+            }, interval);
+        };
 
-    animate(3, setExperience);
-    animate(150, setProjects);
-    animate(250, setClients);
-    animate(24, setTasks);
-    animate(7, setSupport);
-  }, []);
+        animate(15, setExperience);
+        animate(50, setProjects);
+        animate(99.9, setClients);
+        animate(24, setTasks);
+        animate(7, setSupport);
+    }, []);
 
-  return (
-    <section className="bg-black text-white py-20">
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-8 text-center">
+    return (
+        <header>
+            <div className="w-full h-[614px] bg-[#0F131D]">
+                <div className="container flex flex-col justify-center h-full">
+                    <div className="flex gap-[18px] flex-col h-[310px]">
+                        <div className="w-[193px] h-[22px] border-[2px] border-[#ADC6FF] rounded-[99999px] bg-[#0F131D] text-[12px] flex items-center justify-center text-[#ADC6FF] font-semibold">
+                            <p>ENGINEERING THE FUTURE</p>
+                        </div>
+                        <div className="flex flex-col gap-[18px]">
+                            <h1 className="text-white text-[72px] font-bold leading-[80px] max-w-[735px] w-full">
+                                The Minds Behind
+                                <span className="text-[#ADC6FF]"> The Code.</span>
+                            </h1>
+                            <p className="text-[#C2C6D6] text-[19px] w-[543px]">
+                                We are a collective of high-performance engineers dedicated to
+                                building the backbone of the next digital era through precision,
+                                speed, and luxury design.</p>
+                        </div>
+                        div
+                    </div> 
+                </div>
+            </div>
+        </header>
+
+
+            /* <div className="grid grid-cols-2 md:grid-cols-4 gap-8 text-center">
         <div>
           <h2 className="text-5xl font-bold">{experience}</h2>
           <p>Years Experience</p>
@@ -48,7 +71,7 @@ export default function About() {
         </div>
 
         <div>
-          <h2 className="text-5xl font-bold">{clients}+</h2>
+          <h2 className="text-5xl font-bold">{clients}%</h2>
           <p>Happy Clients</p>
         </div>
 
@@ -56,7 +79,7 @@ export default function About() {
           <h2 className="text-5xl font-bold">{tasks}/{support}</h2>
           <p>Support</p>
         </div>
-      </div> 
-    </section>
-  );
+      </div>  */}
+        
+    );
 }
