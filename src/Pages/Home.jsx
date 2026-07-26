3ec5c09bfdb547794ba16bc16f041f37b46b1896
giyosbekganibekov <giyosbekganiybekov2011@gmail.com>
2026-07-27 01:14:22 +0500

    Works section and YT section end, but full home section no end

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,6 +10,15 @@ import Icon3 from "../assets/Icon3.png";
 import Icon4 from "../assets/Icon4.png";
 import Icon5 from "../assets/Icon5.png";
 import Icon6 from "../assets/Icon6.png";
+import WorksImg1 from "../assets/WorksImg1.png";
+import WorksImg2 from "../assets/WorksImg2.png";
+import WorksImg3 from "../assets/WorksImg3.png";
+import WorksIcon1 from "../assets/WorksIcon1.png";
+import WorksIcon2 from "../assets/WorksIcon2.png";
+import WorksIcon3 from "../assets/WorksIcon3.png";
+import YtIcon1 from "../assets/YtIcon1.png";
+import YtImg1 from "../assets/YtImg1.png";
+
 
 function Home() {
     const [count, setCount] = useState(0);
@@ -102,7 +111,191 @@ function Home() {
                     </div>
                 </div>
             </section>
-        </div>
+
+
+            <section>
+                <div className="bg-[#171B26] py-[80px]">
+                    <div className="container mx-[32px]">
+                        <div>
+                            <div className="h-[102px] flex flex-col gap-[15px] items-center justify-center mb-[80px]">
+                                <h2 className="text-[48px] text-[#DFE2F1] font-semibold">Selected Works</h2>
+                                <p className="text-[#C2C6D6] text-[18px]">A portfolio of engineering excellence.</p>
+                            </div>
+                            <div className="container flex gap-[41px] mt-[32px]">
+                                <div className="container">
+                                    <div className="">
+                                        <div className="relative w-[802px] h-[450px] overflow-hidden rounded-[12px]">
+
+                                            <img
+                                                src={WorksImg1}
+                                                alt="Work 1"
+                                                className="w-[802px] h-[450px] object-cover rounded-[12px]"
+                                            />
+
+                                            {/* Gradient */}
+                                            <div className="absolute inset-0 rounded-[12px] bg-gradient-to-t from-[#0B1120] via-[#0B1120]/50 to-transparent"></div>
+
+                                            {/* Content */}
+                                            <div className="absolute bottom-6 left-6 z-10">
+                                                <h2 className="text-4xl font-bold text-white">
+                                                    Lumina App
+                                                </h2>
+
+                                                <div className="flex gap-2 mt-3">
+                                                    <span className="px-3 py-1 bg-[#ADC6FF]/20 text-[#ADC6FF] rounded-full border border-[#ADC6FF]/30 text-xs">
+                                                        FINTECH
+                                                    </span>
+
+                                                    <span className="px-3 py-1 bg-[#ADC6FF]/20 text-[#ADC6FF] rounded-full border border-[#ADC6FF]/30 text-xs">
+                                                        WEBGL
+                                                    </span>
+                                                </div>
+
+                                                <p className="mt-4 text-[#D1D5DB] max-w-md">
+                                                    Next-generation asset management platform featuring real-time
+                                                    data visualization and biometric security.
+                                                </p>
+                                            </div>
+
+                                            {/* Iconlar */}
+                                            <div className="absolute bottom-6 right-6 flex gap-3 z-10">
+                                                <a href="#!">
+                                                    <button className="w-12 h-12 rounded-full border border-[#ADC6FF]/30 bg-[#111827]/70 backdrop-blur-md flex items-center justify-center hover:bg-[#1F2937] duration-300">
+                                                        <img src={WorksIcon1} alt="Work Icon 1" />
+                                                    </button>
+                                                </a>
+                                                <a href="#!">
+                                                    <button className="w-12 h-12 rounded-full bg-[#ADC6FF] flex items-center justify-center hover:scale-110 duration-300">
+                                                        <img src={WorksIcon2} alt="Work Icon 2" />
+                                                    </button>
+                                                </a>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="container">
+                                    <div className="">
+                                        <div className="relative w-[370px] h-[450px] overflow-hidden rounded-[12px]">
+
+                                            <img
+                                                src={WorksImg2}
+                                                alt="Work 2"
+                                                className="w-[370px] h-[450px] object-cover rounded-[12px]"
+                                            />
+
+                                            {/* Gradient */}
+                                            <div className="w-[370px] h-[450px] absolute inset-0 rounded-[12px] bg-gradient-to-t from-[#0B1120] via-[#0B1120]/50 to-transparent"></div>
+
+                                            {/* Content */}
+                                            <div className="absolute bottom-6 left-6 z-10">
+                                                <h2 className="text-[24px] font-bold text-white">
+                                                    Velocity CRM
+                                                </h2>
+                                                <p className="mt-[8px] text-[#D1D5DB] max-w-[261px]">
+                                                    SaaS solution for hyper-scaling sales
+                                                    teams with integrated automation.
+                                                </p>
+                                                <a href="#">
+                                                    <div className="flex gap-[8px] h-[24px] mt-[24px]">
+                                                        <p className="text[16px] text-[#ADC6FF] font-bold">View Case Study</p>
+                                                        <img className="w-[16px] h-[16px] mt-[7px]" src={WorksIcon3} alt="Work Icon 3" />
+                                                    </div>
+                                                </a>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="mt-[41px]">
+                                <div className="container">
+                                    <div className="">
+                                        <div className="relative w-[1214px] h-[398px] overflow-hidden rounded-[12px]">
+
+                                            <img
+                                                src={WorksImg3}
+                                                alt="Work 1"
+                                                className="w-[1214px] h-[398px] object-cover rounded-[12px]"
+                                            />
+
+                                            {/* Gradient */}
+                                            <div className="absolute inset-0 rounded-[12px] bg-gradient-to-t from-[#0B1120] via-[#0B1120]/50 to-transparent"></div>
+
+                                            {/* Content */}
+                                            <div className=" absolute bottom-[61px] left-[80px] z-10">
+                                                <p className="text-[14px] text-[#ADC6FF]">FLAGSHIP PROJECT</p>
+
+                                                <h2 className="text-[48px] mt-[16px] font-bold text-white">
+                                                    Nexus AI
+                                                </h2>
+
+                                                <p className="mt-[24px] text-[#D1D5DB] text-[18px] max-w-[512px] h-[134px]">
+                                                    A custom-built LLM agent platform for enterprise
+                                                    knowledge bases, reducing query response times by 80%
+                                                    using advanced RAG pipelines.
+                                                </p>
+                                                <div className="flex gap-[24px]">
+                                                    <div className="w-[58px] h-[22px] justify-center items-center flex gap-[8px]">
+                                                        <div className="w-[4px] h-[4px] rounded-full bg-[#ADC6FF]"></div>
+                                                        <p className="text-[#C2C6D6] text-[14px]">Python</p>
+                                                    </div>
+                                                    <div className="w-[82px] h-[22px] justify-center items-center flex gap-[8px]">
+                                                        <div className="w-[4px] h-[4px] rounded-full bg-[#ADC6FF]"></div>
+                                                        <p className="text-[#C2C6D6] text-[14px]">LangChain</p>
+                                                    </div>
+                                                    <div className="w-[72px] h-[22px] justify-center items-center flex gap-[8px]">
+                                                        <div className="w-[4px] h-[4px] rounded-full bg-[#ADC6FF]"></div>
+                                                        <p className="text-[#C2C6D6] text-[14px]">Pinecone</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            <section>
+                <div className="bg-[#1C1F2A] max-w-full h-[480px]">
+                    <div className="container py-[80px]">
+                        <div className="flex gap-[85px]">
+                            <div className="w-[568px] h-[270px]">
+                                <h2 className="text-[48px] text-[#DFE2F1] font-semibold">
+                                    Peek Behind the Code
+                                </h2>
+                                <p className="text-[#C2C6D6] text-[18px] mt-[23px]">
+                                    Explore our engineering process, project breakdown, and
+                                    developer culture on our YouTube channel. We share insights to
+                                    empower the global developer community.
+                                </p>
+                                <a
+                                    href="https://www.youtube.com/@AZIZBEK_KAMILOV"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="inline-flex w-[297px] h-[72px] items-center justify-center gap-3 rounded-[12px] bg-red-600 hover:bg-red-700 text-white font-bold mt-8"
+                                >
+                                    <img src={YtIcon1} alt="YouTube Icon" />
+                                    <span>Subscribe to Channel</span>
+                                </a>
+                            </div>
+                            <div>
+                                <a className="" href="https://youtu.be/RS2aCn-mBvA" target="_blank" rel="noopener noreferrer">
+                                    <img className="" src={YtImg1} alt="YouTube Thumbnail" />
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </div >
     );
 }
 
