98a7e0ce79ad56f31989b139944c6d2f5bbc51b2
sobirjon <sobirjonotajonov11@gmail.com>
2026-07-30 13:24:59 +0500

    for now

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,5 +1,7 @@
 import card1 from "../assets/card1.png";
 import card2 from "../assets/card2.png";
+import racket from "../assets/racket.png";
+import card from "../assets/cardd.png";
 
 function Projects() {
   return (
@@ -47,72 +49,271 @@ function Projects() {
             <div className="bg-[#111827B2] w-[802px]">
               <img src={card1} alt="" />
               <div className="px-[24px] py-[23px]">
-                <h2 className="text-[#DFE2F1] text-[24px] font-medium mb-[8px]">Lumina App</h2>
-                    <p className="text-[#C2C6D6] text-[16px] font-normal mb-[24px]">
+                <h2 className="text-[#DFE2F1] text-[24px] font-medium mb-[8px]">
+                  Lumina App
+                </h2>
+                <p className="text-[#C2C6D6] text-[16px] font-normal mb-[24px]">
                   A holistic wellness ecosystem focusing on biometric
                   integration and AI-driven health optimization. Designed for
                   2.5M+ active users.
                 </p>
-                <span className="flex gap-[16px]"> 
-                        <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[97px] rounded-[4px]">React Native</p>
-                        <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[72px] rounded-[4px]">GraphQL</p>
-                        <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[45px] rounded-[4px]">AWS</p>
+                <span className="flex gap-[16px]">
+                  <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[97px] rounded-[4px]">
+                    React Native
+                  </p>
+                  <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[72px] rounded-[4px]">
+                    GraphQL
+                  </p>
+                  <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[45px] rounded-[4px]">
+                    AWS
+                  </p>
                 </span>
               </div>
             </div>
             <div className="bg-[#111827B2] w-[389px]">
               <img src={card2} alt="" />
               <div className="px-[24px] py-[23px]">
-                <h2 className="text-[#DFE2F1] text-[24px] font-medium mb-[8px]">Lumina App</h2>
-                    <p className="text-[#C2C6D6] text-[16px] font-normal mb-[24px]">
+                <h2 className="text-[#DFE2F1] text-[24px] font-medium mb-[8px]">
+                  Lumina App
+                </h2>
+                <p className="text-[#C2C6D6] text-[16px] font-normal mb-[24px]">
                   A holistic wellness ecosystem focusing on biometric
                   integration and AI-driven health optimization. Designed for
                   2.5M+ active users.
                 </p>
-                <span className="flex gap-[16px]"> 
-                        <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[97px] rounded-[4px]">React Native</p>
-                        <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[72px] rounded-[4px]">GraphQL</p>
-                        <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[45px] rounded-[4px]">AWS</p>
+                <span className="flex gap-[16px]">
+                  <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[97px] rounded-[4px]">
+                    React Native
+                  </p>
+                  <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[72px] rounded-[4px]">
+                    GraphQL
+                  </p>
+                  <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[45px] rounded-[4px]">
+                    AWS
+                  </p>
                 </span>
               </div>
             </div>
-
           </div>
           <div className="flex gap-[25px] mt-[24px]">
-             <div className="bg-[#111827B2] w-[389px]">
+            <div className="bg-[#111827B2] w-[389px]">
               <img src={card2} alt="" />
               <div className="px-[24px] py-[23px]">
-                <h2 className="text-[#DFE2F1] text-[24px] font-medium mb-[8px]">Lumina App</h2>
-                    <p className="text-[#C2C6D6] text-[16px] font-normal mb-[24px]">
+                <h2 className="text-[#DFE2F1] text-[24px] font-medium mb-[8px]">
+                  Lumina App
+                </h2>
+                <p className="text-[#C2C6D6] text-[16px] font-normal mb-[24px]">
                   A holistic wellness ecosystem focusing on biometric
                   integration and AI-driven health optimization. Designed for
                   2.5M+ active users.
                 </p>
-                <span className="flex gap-[16px]"> 
-                        <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[97px] rounded-[4px]">React Native</p>
-                        <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[72px] rounded-[4px]">GraphQL</p>
-                        <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[45px] rounded-[4px]">AWS</p>
+                <span className="flex gap-[16px]">
+                  <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[97px] rounded-[4px]">
+                    React Native
+                  </p>
+                  <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[72px] rounded-[4px]">
+                    GraphQL
+                  </p>
+                  <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[45px] rounded-[4px]">
+                    AWS
+                  </p>
                 </span>
               </div>
             </div>
             <div className="bg-[#111827B2] w-[802px]">
               <img src={card1} alt="" />
               <div className="px-[24px] py-[23px]">
-                <h2 className="text-[#DFE2F1] text-[24px] font-medium mb-[8px]">Lumina App</h2>
-                    <p className="text-[#C2C6D6] text-[16px] font-normal mb-[24px]">
+                <h2 className="text-[#DFE2F1] text-[24px] font-medium mb-[8px]">
+                  Lumina App
+                </h2>
+                <p className="text-[#C2C6D6] text-[16px] font-normal mb-[24px]">
                   A holistic wellness ecosystem focusing on biometric
                   integration and AI-driven health optimization. Designed for
                   2.5M+ active users.
                 </p>
-                <span className="flex gap-[16px]"> 
-                        <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[97px] rounded-[4px]">React Native</p>
-                        <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[72px] rounded-[4px]">GraphQL</p>
-                        <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[45px] rounded-[4px]">AWS</p>
+                <span className="flex gap-[16px]">
+                  <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[97px] rounded-[4px]">
+                    React Native
+                  </p>
+                  <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[72px] rounded-[4px]">
+                    GraphQL
+                  </p>
+                  <p className="text-[#C2C6D6] text-[14px] font-normal bg-[#262A35] px-[8px] py-[4px] w-[45px] rounded-[4px]">
+                    AWS
+                  </p>
                 </span>
               </div>
             </div>
+          </div>
+        </section>
+        <section className="container">
+          <div className="mt-[48px] flex gap-[24px]">
+            <div className="flex bg-[#111827B2] w-[813px] justify-between p-[48px] rounded-[12px] border border-[#FFFFFF1A]">
+              <div>
+                <img src={racket} alt="" />
+                <h2 className="text-[#DFE2F1] text-[24px] font-medium mt-[23px]">
+                  Web Platforms
+                </h2>
+                <p className="text-[16px] text-[#C2C6D6] font-normal w-[400px] mt-[8px]">
+                  Hyper-fast, SEO-optimized web experiences built for
+                  high-conversion and industrial scale.
+                </p>
+                <div className="flex gap-[8px] mt-[24px]">
+                  <p className="text-[#DFE2F1] text-[12px] font-normal bg-[#313540] px-[8px] py-[4px] border border-[#42475466] rounded-[4px] w-[57px] text-center">
+                    Next.js
+                  </p>
+                  <p className="text-[#DFE2F1] text-[12px] font-normal bg-[#313540] px-[8px] py-[4px] border border-[#42475466] rounded-[4px] w-[51px] text-center">
+                    React
+                  </p>
+                  <p className="text-[#DFE2F1] text-[12px] font-normal bg-[#313540] px-[8px] py-[4px] border border-[#42475466] rounded-[4px] w-[77px] text-center">
+                    TypeScript
+                  </p>
+                  <p className="text-[#DFE2F1] text-[12px] font-normal bg-[#313540] px-[8px] py-[4px] border border-[#42475466] rounded-[4px] w-[63px] text-center">
+                    Tailwind
+                  </p>
+                </div>
+              </div>
+              <div>
+                <img src={card} alt="" />
+              </div>
+            </div>
+            <div className="flex bg-[#111827B2] w-[394px] justify-between p-[48px] rounded-[12px] border border-[#FFFFFF1A]">
+              <div>
+                <img src={racket} alt="" />
+                <h2 className="text-[#DFE2F1] text-[24px] font-medium mt-[23px]">
+                  UI/UX Design
+                </h2>
+                <p className="text-[16px] text-[#C2C6D6] font-normal w-[297px] mt-[8px]">
+                  Immersive interfaces that balance aesthetic beauty with
+                  functional clarity.
+                </p>
+                <div className="flex justify-between items-center mt-[24px]">
+                  <p className="text-[#ADC6FF] text-[12px] font-normal">
+                    FIGMA / ADOBE CC / FRAMER
+                  </p>
+                  <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    value="70"
+                    class="w-[64px] h-[10px] rounded-[20px] bg-[#2b3350] accent-[#a7c1fa]"
+                  ></input>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-[48px] flex gap-[24px]">
+             <div className="flex bg-[#111827B2] w-[394px] justify-between p-[48px] rounded-[12px] border border-[#FFFFFF1A]">
+              <div>
+                <img src={racket} alt="" />
+                <h2 className="text-[#DFE2F1] text-[24px] font-medium mt-[23px]">
+                  UI/UX Design
+                </h2>
+                <p className="text-[16px] text-[#C2C6D6] font-normal w-[297px] mt-[8px]">
+                  Immersive interfaces that balance aesthetic beauty with
+                  functional clarity.
+                </p>
+                <div className="flex justify-between items-center mt-[24px]">
+                  <p className="text-[#ADC6FF] text-[12px] font-normal">
+                    FIGMA / ADOBE CC / FRAMER
+                  </p>
+                  <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    value="70"
+                    class="w-[64px] h-[10px] rounded-[20px] bg-[#2b3350] accent-[#a7c1fa]"
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="flex bg-[#111827B2] w-[813px] justify-between p-[48px] rounded-[12px] border border-[#FFFFFF1A]">
+              <div>
+                <img src={racket} alt="" />
+                <h2 className="text-[#DFE2F1] text-[24px] font-medium mt-[23px]">
+                  Web Platforms
+                </h2>
+                <p className="text-[16px] text-[#C2C6D6] font-normal w-[400px] mt-[8px]">
+                  Hyper-fast, SEO-optimized web experiences built for
+                  high-conversion and industrial scale.
+                </p>
+                <div className="flex gap-[8px] mt-[24px]">
+                  <p className="text-[#DFE2F1] text-[12px] font-normal bg-[#313540] px-[8px] py-[4px] border border-[#42475466] rounded-[4px] w-[57px] text-center">
+                    Next.js
+                  </p>
+                  <p className="text-[#DFE2F1] text-[12px] font-normal bg-[#313540] px-[8px] py-[4px] border border-[#42475466] rounded-[4px] w-[51px] text-center">
+                    React
+                  </p>
+                  <p className="text-[#DFE2F1] text-[12px] font-normal bg-[#313540] px-[8px] py-[4px] border border-[#42475466] rounded-[4px] w-[77px] text-center">
+                    TypeScript
+                  </p>
+                  <p className="text-[#DFE2F1] text-[12px] font-normal bg-[#313540] px-[8px] py-[4px] border border-[#42475466] rounded-[4px] w-[63px] text-center">
+                    Tailwind
+                  </p>
+                </div>
+              </div>
+              <div>
+                <img src={card} alt="" />
+              </div>
+            </div>
            
-
+          </div>
+          <div className="mt-[48px] flex gap-[24px]">
+             <div className="flex bg-[#111827B2] w-[813px] justify-between p-[48px] rounded-[12px] border border-[#FFFFFF1A]">
+              <div>
+                <img src={racket} alt="" />
+                <h2 className="text-[#DFE2F1] text-[24px] font-medium mt-[23px]">
+                  Web Platforms
+                </h2>
+                <p className="text-[16px] text-[#C2C6D6] font-normal w-[400px] mt-[8px]">
+                  Hyper-fast, SEO-optimized web experiences built for
+                  high-conversion and industrial scale.
+                </p>
+                <div className="flex gap-[8px] mt-[24px]">
+                  <p className="text-[#DFE2F1] text-[12px] font-normal bg-[#313540] px-[8px] py-[4px] border border-[#42475466] rounded-[4px] w-[57px] text-center">
+                    Next.js
+                  </p>
+                  <p className="text-[#DFE2F1] text-[12px] font-normal bg-[#313540] px-[8px] py-[4px] border border-[#42475466] rounded-[4px] w-[51px] text-center">
+                    React
+                  </p>
+                  <p className="text-[#DFE2F1] text-[12px] font-normal bg-[#313540] px-[8px] py-[4px] border border-[#42475466] rounded-[4px] w-[77px] text-center">
+                    TypeScript
+                  </p>
+                  <p className="text-[#DFE2F1] text-[12px] font-normal bg-[#313540] px-[8px] py-[4px] border border-[#42475466] rounded-[4px] w-[63px] text-center">
+                    Tailwind
+                  </p>
+                </div>
+              </div>
+              <div>
+                <img src={card} alt="" />
+              </div>
+            </div>
+             <div className="flex bg-[#111827B2] w-[394px] justify-between p-[48px] rounded-[12px] border border-[#FFFFFF1A]">
+              <div>
+                <img src={racket} alt="" />
+                <h2 className="text-[#DFE2F1] text-[24px] font-medium mt-[23px]">
+                  UI/UX Design
+                </h2>
+                <p className="text-[16px] text-[#C2C6D6] font-normal w-[297px] mt-[8px]">
+                  Immersive interfaces that balance aesthetic beauty with
+                  functional clarity.
+                </p>
+                <div className="flex justify-between items-center mt-[24px]">
+                  <p className="text-[#ADC6FF] text-[12px] font-normal">
+                    FIGMA / ADOBE CC / FRAMER
+                  </p>
+                  <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    value="70"
+                    class="w-[64px] h-[10px] rounded-[20px] bg-[#2b3350] accent-[#a7c1fa]"
+                  ></input>
+                </div>
+              </div>
+            </div>
+           
+           
           </div>
         </section>
       </main>
